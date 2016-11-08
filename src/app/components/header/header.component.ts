6ef0f75648323fa33401c60d6279a7c7ea0a389f
Yuvaraj Santhakumar <yuvaraj89@gmail.com>
2016-11-09 00:56:58 +0530

    cleaned up header.component.ts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,31 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  isLinkActive:Array<boolean> = [true,false,false];
-
   constructor() { }
 
   ngOnInit() {
   }
 
-
-  activateLink(linkName:string){
-    if(linkName==='Tournaments'){
-      this.isLinkActive[0]=false;
-      this.isLinkActive[1]=true;
-      this.isLinkActive[2]=false;
-    }
-    else if(linkName==='Fixtures'){
-      this.isLinkActive[0]=true;
-      this.isLinkActive[1]=false;
-      this.isLinkActive[2]=false;
-    }
-    else if(linkName==='Standings'){
-      this.isLinkActive[0]=false;
-      this.isLinkActive[1]=false;
-      this.isLinkActive[2]=true;
-    }
-
-  }
 
 }
