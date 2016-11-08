07481c231aad8681e196c55b52cfb1e220a33e60
Yuvaraj Santhakumar <yuvaraj89@gmail.com>
2016-11-08 21:14:38 +0530

    added components for all pages

---
+++ src/app/components/fixtures/fixtures.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ft-fixtures',
+  templateUrl: './fixtures.component.html',
+  styleUrls: ['./fixtures.component.css']
+})
+export class FixturesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
